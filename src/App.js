465ce0a7955bb1f40bb55b099d565bdb8631dc37
Roshan Madumadawa <roshankulathissa@gmail.com>
2024-01-13 16:31:22 +0530

    add new componets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Contact from "./Components/Contact";
+import Work from "./Components/Work";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      {/* <sdkjfkn? */}
-      {/* jnvfgdlk? */}
+      <Navbar />
+      <Contact />
+      <Work />
     </div>
   );
 }
